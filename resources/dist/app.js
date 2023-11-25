f3cb015bfa0833c9faf7662f475a87a3a61d5522
RD <hello@codesomelabs.com>
2023-11-25 11:32:38 +0200

    feat: Improve keyboard navigation and focus handling
    - Added logic to handle focusing on the next and previous elements based on the given index.
    - Added event listener for the ArrowUp key to focus on a specific element when pressed.
    - Improved code readability by using conditional statements to check if an element exists before focusing on it.

--- resources/dist/app.js
+++ resources/dist/app.js
@@ -1,12 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   if (window.Livewire) {
     Livewire.on('focusNext', (nextIndex) => {
-      document.querySelector(`[tabindex="${nextIndex}"]`).focus();
+      const nextElement = document.querySelector(`[tabindex="${nextIndex}"]`);
+      if (nextElement) {
+        nextElement.focus();
+      }
     });
 
     Livewire.on('focusPrev', (prevIndex) => {
       if (prevIndex >= 0) {
-        document.querySelector(`[tabindex="${prevIndex}"]`).focus();
+        const prevElement = document.querySelector(`[tabindex="${prevIndex}"]`);
+        if (prevElement) {
+          prevElement.focus();
+        }
+      }
+    });
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'ArrowUp') {
+        const firstItem = document.querySelector('[tabindex="0"]');
+        const currentFocus = document.activeElement;
+
+        if (currentFocus === firstItem) {
+          const addressElement = document.getElementById('address');
+          if (addressElement) {
+            addressElement.focus();
+            event.preventDefault();
+          }
+        }
       }
     });
   }
