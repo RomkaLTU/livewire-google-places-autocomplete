1a7a96d20ddc0b6bda365606d4cd4d8f8511e97f
RD <hello@codesomelabs.com>
2023-11-25 11:24:32 +0200

    Refactor field.blade.php for improved autocomplete functionality
    - Added div container to wrap input and autocomplete results
    - Added wire:keydown.escape to clear search input
    - Added wire:keydown.arrow-down to focus on next result
    - Added wire:keydown.arrow-up to focus on previous result
    - Added autocomplete="off" attribute to input element
    - Updated structure of autocomplete results with conditional rendering based on predictions array presence

--- resources/views/field.blade.php
+++ resources/views/field.blade.php
@@ -3,28 +3,37 @@
         <label for="{{ $elementId }}" class="lvgpa-label {{ $labelClass }}">
             {!! $label !!}
         </label>
-        <input
+        <div class="w-full relative">
+          <input
             id="{{ $elementId }}"
             class="lvgpa-input {{ $inputClass }}"
             type="text"
             wire:model.debounce.700ms="search"
+            wire:keydown.escape="clear"
+            wire:keydown.arrow-down="focusNext(-1)"
             name="place"
+            autocomplete="off"
             aria-controls="autocomplete-results" />
-    </div>
-    <div id="lgpa-autocomplete-results" class="lvgpa-results" role="listbox">
-        @forelse($predictions as $index => $prediction)
-            <div
-                role="option"
-                tabindex="{{ $index }}"
-                wire:key="{{ $prediction['place_id'] }}"
-                wire:keydown.arrow-down="focusNext({{ $index }})"
-                wire:keydown.arrow-up="focusPrev({{ $index }})"
-                wire:keydown.enter="selectPlace('{{ $prediction['place_id'] }}')"
-                wire:click="selectPlace('{{ $prediction['place_id'] }}')">
-                {{ $prediction['structured_formatting']['main_text'] }}
+          @if($predictions)
+            <div id="lgpa-autocomplete-results" class="lvgpa-results absolute left-0 w-full bg-white border border-t-none border-gray-500 lvgpa-results divide-y divide-solid divide-gray-400 gap-y-2" role="listbox">
+              @forelse($predictions as $index => $prediction)
+                <div
+                  role="option"
+                  tabindex="{{ $index }}"
+                  class="lvgpa-results-item p-2 cursor-pointer hover:bg-gray-200 p-2 focus:bg-gray-200"
+                  wire:key="{{ $prediction['place_id'] }}"
+                  wire:keydown.escape="clear"
+                  wire:keydown.arrow-down="focusNext({{ $index }})"
+                  wire:keydown.arrow-up="focusPrev({{ $index }})"
+                  wire:keydown.enter="selectPlace('{{ $prediction['place_id'] }}')"
+                  wire:click="selectPlace('{{ $prediction['place_id'] }}')">
+                  {{ $prediction['structured_formatting']['main_text'] }}
+                </div>
+              @empty
+              @endforelse
             </div>
-        @empty
-        @endforelse
+          @endif
+        </div>
     </div>
 
     <link href="{{ asset('vendor/livewire-google-places-autocomplete/main.css') }}" rel="stylesheet">
